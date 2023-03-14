b253eccfef1a16940615793868706bf516ce4cbc
javierbull <javierbullreyes@gmail.com>
2023-03-14 10:54:14 -0500

    update form contact

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-  var image = document.getElementsByClassName('parallax');
-  new simpleParallax(image, {
-  	scale: 1.5
+  // var image = document.getElementsByClassName('parallax');
+  // new simpleParallax(image, {
+  // 	scale: 1.5
+
+  // Show hide select tipo persona
+
+// End Show hide select tipo persona
 });
