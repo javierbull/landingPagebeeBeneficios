99546983c2c4734b573165b9de702cba6e23e5e9
javierbull <javierbullreyes@gmail.com>
2023-03-14 11:50:12 -0500

    function show hide sekect tipo persona

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // var image = document.getElementsByClassName('parallax');
   // new simpleParallax(image, {
   // 	scale: 1.5
 
   // Show hide select tipo persona
+  $("#selectTipoPersona").change(function () {
+    $(this).find("option:selected").each(function () {
+      var optionValue = $(this).attr("value");
+      if (optionValue) {
+        $(".form-box").not("." + optionValue).hide();
+        $("." + optionValue).show();
+      } else {
+        $(".form-box").hide();
+      }
+    });
+  }).change();
+  // End Show hide select tipo persona
 
-// End Show hide select tipo persona
 });
